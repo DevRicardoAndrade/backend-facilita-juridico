2accab0913abdecd11a5d6238fa1b824c9e624c2
Ricardo Andrade <ricardo00.andrade0@gmail.com>
2024-03-08 09:50:04 -0300

    feat: Implementando express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const PORT = process.env.PORT || 3030;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
