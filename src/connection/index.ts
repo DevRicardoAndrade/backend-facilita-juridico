504637941a3fa7d0af0cb4a30b3f4cf1f8d48b95
Ricardo Andrade <ricardo00.andrade0@gmail.com>
2024-03-08 09:50:27 -0300

    feat: Implementando conexão com BD

---
+++ src/connection/index.ts
@@ -0,0 +1,25 @@
+import { Client } from "pg";
+import IConfig from "./IConfig";
+
+const config: IConfig = {
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+};
+
+const client = new Client(config);
+
+const conn = {
+  connect: () => {
+    client.connect();
+  },
+  end: () => {
+    client.end();
+  },
+  querry: async (sqlText: string, parameters?: []) => {
+    return await client.query(sqlText, parameters);
+  },
+};
+
+export default conn;
